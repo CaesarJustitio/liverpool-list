620daea485cba657df26e443f3992c85033054c0
Caesar Justitio <caesarjustitio17@gmail.com>
2023-09-11 13:23:18 +0700

    Menambahkan kode dari file models.py

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    goals = models.IntegerField();
+    marketPrice = models.IntegerField();
